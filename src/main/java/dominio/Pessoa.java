fc3087ea18f54b889ed472a3dfe08cf0f7e9a430
Marcos santos <marcosvn3@academico.ufs.br>
2024-10-10 15:37:49 -0300

    implementando classe pessoa.

---
+++ src/main/java/dominio/Pessoa.java
@@ -0,0 +1,60 @@
+package dominio;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "pessoas_db")
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    private String nome;
+    private String emai;
+
+    public Pessoa() {
+    }
+
+    public Pessoa( String nome, String emai) {
+
+        this.nome = nome;
+        this.emai = emai;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmai() {
+        return emai;
+    }
+
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", emai='" + emai + '\'' +
+                '}';
+    }
+}
